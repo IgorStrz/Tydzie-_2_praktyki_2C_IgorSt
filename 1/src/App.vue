ef3e7d8bcb9f341c5bf5cd10030d9f8dbfe801b0
IgorStrz <igor.strzalkaa@gmail.com>
2025-01-21 11:30:43 +0100

    2

---
+++ 1/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+body{
+  background-color: #A6A6A6;
+}
+#app {
+  font-family: arial;
+  font-size: 20px;
+  color: #0D0D0D;
+  text-align: center;
+}
+</style>
---
+++ 1/src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="galeria">
+    <h1>Galeria zdjęć</h1>
+    <div class="XD">
+      <div v-for="(zdj, i) in zdj" :key="i" class="NG">
+        <p>{{ zdj.title }}</p>
+        <img :src="zdj.url" :alt="zdj.title" />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      zdj: [
+        { url: require('@/assets/Doge.jpg'), title: "Doge" },
+        {url: require('@/assets/BDoge.jpg'), title: "Bread doge" },
+        { url: require('@/assets/SDoge.jpg'), title: "Sad doge" },
+        { url: require('@/assets/GDoge.jpg'), title: "Giga chad doge" },
+      ],
+    };
+  },
+};
+</script>
+<style>
+.galeria{
+  text-align: center;
+  background-color: #A6A6A6;
+}
+.NG{
+  border: 5px solid #E80C4C;
+  overflow: hidden;
+  border-radius: 10px;
+}
+.XD{
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  gap: 10px;
+}
+img{
+  width: 100%;
+  height: auto;
+}
+p{
+  padding: 15px;
+  background-color: #A6A6A6;
+}
+</style>
